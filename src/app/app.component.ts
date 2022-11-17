4595b2eb0df1807c2e5ef73149de8cdc234aee83
Danilo Flores Desktop <dan_eugenio@hotmail.com>
2022-11-16 23:58:59 -0300

    basic steps to read json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Pessoas from './dados/scrumDados.json';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Ihc2022';
+  public pessoaList:{}[] = Pessoas;
 }
