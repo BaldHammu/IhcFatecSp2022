9f427ceb6461973a85713d03115eef8144827c4c
Danilo Flores Desktop <dan_eugenio@hotmail.com>
2022-11-17 00:32:02 -0300

    Leitura do json em objetos publicos + correcoes no json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import Pessoas from './dados/scrumDados.json';
+import dados from './dados/scrumDados.json';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +8,57 @@ import Pessoas from './dados/scrumDados.json';
 })
 export class AppComponent {
   title = 'Ihc2022';
-  public pessoaList:{}[] = Pessoas;
+
+  public pessoas:{
+    ID_pessoa: number;
+    Nome: string;
+  }[] = dados['Pessoas'];
+
+  public projetos:{
+    ID_projeto: number;
+    Nome: string;
+    Descricao: string;
+    Data_inicio: string;
+    Data_entrega: string;
+  }[] = dados['Projetos']
+
+  public sprints:{
+    ID_sprint: number;
+    ID_projeto: number;
+    Descricao: string;
+    Data_inicio: string;
+    Data_fim: string;
+  }[] = dados['Sprints']
+
+  public roles:{
+    ID_role: number;
+    Descricao: string;
+  }[] = dados['Roles']
+
+  public alocacoes:{
+    ID_projeto: number;
+    ID_pessoa: number;
+    Role: number;
+  }[] = dados['Alocacoes']
+
+  public statustarefas:{
+    ID_status: number;
+    Descricao: string;
+  }[] = dados['StatusTarefas']
+
+  public tarefas:{
+    ID_tarefa: number;
+    ID_sprint: number;
+    Descricao: string;
+    Status: number;
+    Data_inicio: string;
+    Data_fim: string;
+  }[] = dados['Tarefas']
+
+  public execucoes:{
+    ID_pessoa: number;
+    ID_projeto: number;
+    ID_tarefa: number;
+  }[] = dados['Execucoes']
+
 }
